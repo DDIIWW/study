f453dae9c9e14f9f1be0661ecf0ee615823989e1
DDIIWW <manymaslov@yandex.ru>
2024-02-03 16:25:45 +0500

    Home_work_5_arrays

--- Lesson_5/Home_work_5_arrays.cpp
+++ Lesson_5/Home_work_5_arrays.cpp
@@ -7,6 +7,7 @@ int task_1(){
                  std::cin>>arr[i];
         }else{arr[i]=arr[i-1]*2;}         
         std::cout<<arr[i]<<" ";}
+    std::cout<<std::endl;
     return 1;}
 
 int task_2(){
@@ -16,6 +17,7 @@ int task_2(){
                  std::cin>>arr[i];
         }else{arr[i]=arr[i-1]+1;}        
         std::cout<<arr[i]<<" ";}
+    std::cout<<std::endl;
     return 2;}
 
 int task_3(){
@@ -60,11 +62,74 @@ int task_3(){
         std::cout<<std::endl;} 
     return 3;}
 
+int task_4(){
+    const int size_arr=3;
+    const int rand_size=100;
+    int sum=0,min=rand_size,max=0;
+    double arithmetic_mean;
+    int arr[size_arr][size_arr];
+    for(int i=0;i<size_arr;i++){//init array
+        for(int j=0;j<size_arr;j++){
+            arr[i][j]=rand()%rand_size;
+            std::cout<<arr[i][j]<<" ";}
+        std::cout<<std::endl;}
+    for(int i=0;i<size_arr;i++){//calculating
+        for(int j=0;j<size_arr;j++){
+            sum+=arr[i][j];
+            if(arr[i][j]>max)max=arr[i][j];
+            if(arr[i][j]<min)min=arr[i][j];}}
+    arithmetic_mean=sum/(size_arr*size_arr);
+    std::cout<<"summa = "<<sum<<"\narithmetic mean = "<<arithmetic_mean<<"\nminimal ="<<min<<"\nmax = "<<max<<std::endl;
+    return 4;}
+
+int task_5(){
+    const int size_arr=3;
+    const int rand_size=15;
+    int arr[size_arr][size_arr];
+    for(int i=0;i<size_arr;i++){//init array
+        for(int j=0;j<size_arr;j++)arr[i][j]=rand()%rand_size;}
+    int vertical_sum[size_arr],horizont_sum[size_arr];
+    for(int i=0;i<size_arr;i++)vertical_sum[i]=0,horizont_sum[i]=0;
+    for(int i=0;i<size_arr;i++){//calculating
+        for(int j=0;j<size_arr;j++){
+            horizont_sum[i]+=arr[i][j];
+            vertical_sum[j]+=arr[i][j];}}
+    int sum=0;    
+    for(int i=0;i<size_arr;i++)sum+=horizont_sum[i];
+    for(int i=0;i<size_arr;i++){
+        for(int j=0;j<size_arr;j++){
+            std::cout<<arr[i][j]<<"  ";}
+        std::cout<<"|"<<horizont_sum[i]<<std::endl;}
+    for(int i=0;i<size_arr*size_arr+1;i++)std::cout<<"-";
+    std::cout<<std::endl;
+    for(int i=0;i<size_arr;i++)std::cout<<vertical_sum[i]<<" ";
+    std::cout<<sum<<std::endl;  
+    return 5;}
+
+int task_6(){
+    int arr_1[5][10];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<10;j++){
+            arr_1[i][j]=rand()%50;
+            std::cout<<arr_1[i][j]<<" ";}
+        std::cout<<std::endl;}
+    std::cout<<std::endl;
+    int arr_2[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0,k=0;k<5;j+=2,k++){
+            arr_2[i][k]=arr_1[i][j]+arr_1[i][j+1];
+            std::cout<<arr_2[i][k]<<" ";}
+        std::cout<<std::endl;}
+    return 6;}
+
 int main(){
     for(int userInput=3;userInput!=0;userInput++){
         std::cout<<"Let's go\nenter the task number\n0-Exit"<<std::endl;
         std::cin>>userInput;
         (userInput==1)? userInput=task_1():
         (userInput==2)? userInput=task_2():
-        (userInput==3)? userInput=task_3():userInput--;}
+        (userInput==3)? userInput=task_3():
+        (userInput==4)? userInput=task_4():
+        (userInput==5)? userInput=task_5():
+        (userInput==6)? userInput=task_6():userInput--;}
     return 0;}
