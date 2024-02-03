2d466e416eb7342452d83ebc0509fe1628050c75
DDIIWW <manymaslov@yandex.ru>
2024-02-03 12:50:38 +0500

    Home_work_5

--- Lesson_5/Home_work_5.cpp
+++ Lesson_5/Home_work_5.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 
 int main(){
-    
-}
+    for(int userInput=3;userInput!=0;userInput++){
+        std::cout<<"Let's go\n0-Exit"<<std::endl;
+        std::cin>>userInput;
+        (userInput==1)? userInput=search_double_array():
+        //(userInput==2)? userInput=X_or_0():userInput=-1;}
+    return 0;}
